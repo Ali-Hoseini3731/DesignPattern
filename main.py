f7dac2f1f83278ea67061988fd327e189f8071a2
Ali Hoseini <a.h.computer73@gmail.com>
2024-06-09 10:42:47 +0330

    factory

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from singleton import Singleton
 
-if __name__ == "__main__":
-    s1 = Singleton()
-    s2 = Singleton()
 
-    print(f"{id(s1)}------{id(s2)}--------{id(s1) == id(s2)}")
--- singleton.py
+++ singleton.py
@@ -7,5 +7,9 @@ class Singleton:
             cls.instance = super().__new__(*args, **kwargs)
         return cls.instance
 
-    def __int__(self):
-        pass
+
+if __name__ == "__main__":
+    s1 = Singleton()
+    s2 = Singleton()
+
+    print(f"{id(s1)}------{id(s2)}--------{id(s1) == id(s2)}")
